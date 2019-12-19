eaa26453ec15a191f331506368ec75f1a428ae77
Florian Schildwaechter <florian.schildwaechter@bauerxcel.de>
2019-12-19 16:47:50 +0100

    refactored fileupload handler

--- router/index.js
+++ router/index.js
@@ -1,15 +1,5 @@
 const { PostController, PageController, } = require("../controller")
-const multer = require("multer")
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads")
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + "-" + Date.now())
-  },
-})
-
-const upload = multer({ storage: storage, })
+const { upload, } = require("../util/uploads")
 
 module.exports = app => {
   // Create a Post
