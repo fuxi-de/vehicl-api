565f33cde01e04e39145ff3f6f536ae71f06e5d7
Florian Schildwaechter <florian.schildwaechter@bauerxcel.de>
2019-12-19 10:46:48 +0100

    eslint config

--- model/Page.js
+++ model/Page.js
@@ -3,11 +3,11 @@ const Schema = mongoose.Schema
 
 const PageSchema = new Schema(
   {
-    headline: { type: String, required: true },
-    body: { type: Array, required: true }
+    headline: { type: String, required: true, },
+    body: { type: Array, required: true, },
   },
   {
-    timestamps: true
+    timestamps: true,
   }
 )
 
