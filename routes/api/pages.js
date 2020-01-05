dad8134a8e49d5406a1b9f0557b4034c9829dd83
Florian Schildwaechter <florian.schildwaechter@bauerxcel.de>
2020-01-05 22:18:31 +0100

    cleaned up routes

--- routes/api/pages.js
+++ routes/api/pages.js
@@ -2,15 +2,15 @@ const router = require("express").Router();
 const { PageController, } = require("../../controller")
 const { upload, } = require("../../util/uploads")
 
-router.get("/pages", PageController.find)
+router.get("/all", PageController.find)
 
-router.post("/pages/create", PageController.create)
+router.post("/create", PageController.create)
 
-router.get("/pages/:id", PageController.findOne)
+router.get("/:id", PageController.findOne)
 
-router.put("/pages/:id/update", PageController.update)
+router.put("/:id/update", PageController.update)
 
-router.delete("/pages/:id/delete", PageController.destroy)
+router.delete("/:id/delete", PageController.destroy)
 
 router.post("/uploadfile", upload.single("myFile"), (req, res, next) => {
   const file = req.file
